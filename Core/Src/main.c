18fa5989e5b1bafa34f0e716640726b0c20a6705
nanogens <nanogens@yahoo.com>
2026-07-03 05:22:46 -0400

    Low Power: Reduce idle sleep inactivity watchdog timeout from 60 seconds to 30 seconds

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -366,7 +366,7 @@ int main(void)
     // Sleep Watchdog
     if (RecordState.started == RECORDING_NOTSTARTED)
     {
-        if ((int32_t)(HAL_GetTick() - last_activity_time) > 60000) // 1 minute (60000 ms)
+        if ((int32_t)(HAL_GetTick() - last_activity_time) > 30000) // 30 seconds (30000 ms)
         {
             if (ApplyRecordingPlan.run == PLAN_RUN_YES)
             {
